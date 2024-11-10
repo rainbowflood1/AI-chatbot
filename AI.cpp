e35a7984a4110625829868fba432e698ea1e8530
Wolfy And Mac <88160870+rainbowflood1@users.noreply.github.com>
2024-11-09 22:18:11 -0500

    Update and rename AI.h to AI.cpp

---
+++ AI.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#define MAX_TOKEN 25
+
+class Neron {
+public:
+    float value;
+    float weight;
+    float bias;
+    float sum() {
+       return (self->value * self->weight) + self->bias;
+    }
+};
+
+class layer {
+    std::vector<Neron>* nerons;
+};
+
+class NuralNetwork {
+    layer input_layer;
+    std::vector<layer>* hidden_layers;
+    layer output_layer;
+};
+
+NuralNetwork create_network() {
+
+}
+
+class AI_chatbot { // Create chatbot instance
+public:
+    std::string input;
+    std::string* tokens;
+}
+
+int main() {
+    
+}
